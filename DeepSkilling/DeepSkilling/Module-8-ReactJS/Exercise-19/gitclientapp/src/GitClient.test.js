c55b3748b97b0193733b92ec5b560bf1a71885ea
likhitha-chandra22 <likhithachnadra6@gmail.com>
2026-07-23 00:59:38 +0530

    Completed ReactJS Exercise 19

---
+++ DeepSkilling/DeepSkilling/Module-8-ReactJS/Exercise-19/gitclientapp/src/GitClient.test.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import GitClient from "./GitClient";
+
+jest.mock("axios");
+
+describe("Git Client Tests", () => {
+
+  test("should return repository names for techiesyed", async () => {
+
+    const repositories = [
+      { name: "Repo1" },
+      { name: "Repo2" }
+    ];
+
+    axios.get.mockResolvedValue({
+      data: repositories
+    });
+
+    const response = await GitClient.getRepositories("techiesyed");
+
+    expect(response.data).toEqual(repositories);
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://api.github.com/users/techiesyed/repos"
+    );
+
+  });
+
+});
